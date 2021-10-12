69fe1fd8025043594ef6564ef6f4f3534d07e93f
ubsisalmanalfarisi <salman.alfarisi.purwakarta@gmail.com>
2021-10-12 15:12:04 +0700

    Menambah Fungsi Max_Length Pada Matakuliah.php

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -7,11 +7,24 @@ $this->load->view('view-form-matakuliah');
 }
 public function cetak()
 {
-$data = [
-'kode' => $this->input->post('kode'),
-'nama' => $this->input->post('nama'),
-'sks' => $this->input->post('sks')
-];
-$this->load->view('view-data-matakuliah', $data);
+	$this->form_validation->set_rules('kode', 'Kode Matakuliah','required|min_length[3]|max_length[4]',
+		['required' => 'Kode Matakuliah Harus diisi',
+		'min_lenght' => 'Kode Matakuliah Terlalu Pendek (Tidak Boleh Kurang dari 3 Karakter)',
+		'max_length' => 'Kode Matakuliah Terlalu Panjang (Karakter Tidak Boleh Lebih Dari 4) '	]);
+	$this->form_validation->set_rules('nama', 'Nama Matakuliah','required|min_length[3]|max_length[20]',	
+	['required' => 'Nama Matakuliah Harus diisi',
+	'min_lenght' => 'Nama Matakuliah Terlalu Pendek (Tidak Boleh Kurang dari 3 Karakter)',
+	'max_length' => 'Nama Matakuliah Terlalu Panjang (Karakter Tidak Boleh Lebih Dari 20)'
+]);
+	if ($this->form_validation->run() != true) {
+	$this->load->view('view-form-matakuliah');
+	} else {
+	$data = [
+	'kode' => $this->input->post('kode'),
+	'nama' => $this->input->post('nama'),
+	'sks' => $this->input->post('sks')
+	];
+	$this->load->view('view-data-matakuliah', $data);
+	}
 }
 }
